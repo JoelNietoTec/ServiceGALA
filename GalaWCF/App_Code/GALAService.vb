3ea7ff610caa9485fba5558442b5f3f4f9909c3c
JoelNietoTec <joelnietotec@outlook.com>
2016-03-10 18:18:33 -0500

    Initvsdvdv

---
+++ GalaWCF/App_Code/GALAService.vb
@@ -0,0 +1,39 @@
+﻿Imports GALAModels
+Imports System.Data.EntityClient
+
+Public Class GALAService
+    Implements IGALAService
+
+    Public Function GetAllClients() As List(Of ClientsData) Implements IGALAService.GetAllClients
+        Dim contexto As GALA_ServiceEntities = New GALA_ServiceEntities()
+        Using contexto
+
+            Dim MyClients As List(Of ClientsData) = New List(Of ClientsData)
+
+            For Each c In contexto.Clientes
+                Dim cData As ClientsData = New ClientsData()
+
+                cData.Nombre = c.Nombre
+                cData.NombreFact = c.NombreFact
+                cData.DireccionCobro = c.DireccionCobro
+                cData.DireccionEnviar = c.DireccionEnviar
+                cData.CodPais = c.CodPais
+                cData.Ter = c.Ter
+                cData.Telefono = c.Telefono
+                cData.Fax = c.Fax
+                cData.CodEmpleado = c.CodEmpleado
+                cData.FechaApertura = c.FechaApertura
+                cData.Email = c.Email
+                cData.CodGrupo = c.CodGrupo
+                cData.ClasificacionIndustria = c.ClasificacionIndustria
+                cData.Observaciones = c.Observaciones
+                cData.CodClase = c.CodClase
+                cData.FechaModificacion = c.FechaModificacion
+                MyClients.Add(cData)
+            Next
+
+            Return MyClients
+        End Using
+
+    End Function
+End Class
---
+++ GalaWCF/App_Code/IGALAService.vb
@@ -0,0 +1,45 @@
+﻿<ServiceContract()>
+Public Interface IGALAService
+    <OperationContract()>
+    Function GetAllClients() As List(Of ClientsData)
+
+
+End Interface
+
+<DataContract()>
+Public Class ClientsData
+    <DataMember()>
+    Public Property Nombre() As String
+    <DataMember()>
+    Public Property NombreFact() As String
+    <DataMember()>
+    Public Property DireccionCobro() As String
+    <DataMember()>
+    Public Property DireccionEnviar() As String
+    <DataMember()>
+    Public Property CodPais() As String
+    <DataMember()>
+    Public Property Ter As Integer
+    <DataMember()>
+    Public Property Telefono() As String
+    <DataMember()>
+    Public Property Fax() As String
+    <DataMember()>
+    Public Property CodEmpleado() As Integer
+    <DataMember()>
+    Public Property FechaApertura() As DateTime
+    <DataMember()>
+    Public Property Email() As String
+    <DataMember()>
+    Public Property CodGrupo() As Integer
+    <DataMember()>
+    Public Property ClasificacionIndustria() As String
+    <DataMember()>
+    Public Property Observaciones() As String
+    <DataMember()>
+    Public Property CodClase() As String
+    <DataMember()>
+    Public Property FechaModificacion() As DateTime
+
+End Class
+
